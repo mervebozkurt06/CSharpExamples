95e6743d9d35f6c66f0462bd1d292c77f60235c5
mervebozkurt06 <mervebzkrts@gmail.com>
2021-01-27 15:29:56 +0300

    Example of dictionary class

---
+++ MyDictionary/MyDic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDic<T>
+    {
+        T[] items;
+        //constructor
+        public MyDic()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] _tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                items[i] = _tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return Items; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDic<string> cities = new MyDic<string>();
+            cities.Add("Boston");
+
+            Console.WriteLine(cities.Length);
+
+            cities.Add("New York");
+
+            Console.WriteLine(cities.Length);
+
+            
+        }
+    }
+}
